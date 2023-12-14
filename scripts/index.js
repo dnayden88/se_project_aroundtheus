24d60a532244fea8fb3b6053ff9d1bcb56d9d69e
Donka Naydenova <dnayden88@gmail.com>
2023-12-14 16:42:37 -0500

    Update

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-let initialCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
@@ -25,53 +25,49 @@ let initialCards = [
   },
 ];
 
-//select the element from the DOM using querySelector
-//console.log(profileEditBtn); checks to see if you have selected the correct element
-//on this element listen for an event
-
 const profileEditBtn = document.querySelector("#profile__edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const profileCloseBtn = document.querySelector("#profile-close-button");
-const profileFormElement = document.querySelector("#modal-form");
+const profileFormElement = document.querySelector("#profile-edit-modal-form");
 const nameInput = document.querySelector("#profile-title-input");
-//select the nameInput
 const jobInput = document.querySelector("#profile-description-input");
-//selects the profile description input
 const profileName = document.querySelector(".profile__title");
-//selects current profile name//
 const profileJob = document.querySelector(".profile__description");
-//selects current profile description//
-const profileEditForm = profileEditModal.querySelector("#modal-form");
-//selecting within the modal NOT the whole document
+const profileEditForm = profileEditModal.querySelector(
+  "#profile-edit-modal-form"
+);
 const template = document.querySelector("#template").content;
 const cardListEl = document.querySelector(".card__gallery");
-console.log(profileName.textContent);
-//get the text content in profileName
-console.log(profileJob.textContent);
-//get the text content in profileDescription
 
-function closePopUp() {
-  profileEditModal.classList.remove("modal__opened");
+function closeModal() {
+  profileEditModal.classList.remove("modal_opened");
 }
-//functions allow for code to be resused and this will be reused. Recommended to create a function to
-//close mulitple pop ups. We only have one pop up right now.
+function openModal() {
+  profileEditModal.classList.add("modal_opened");
+}
 
-profileEditBtn.addEventListener("click", () => {
-  profileEditModal.classList.add("modal__opened");
+function fillProfileForm() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
+}
+
+function openEditProfileModal() {
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+}
+
+profileEditBtn.addEventListener("click", () => {
+  openModal();
+  fillProfileForm();
   //set input fields to have the value, text content pulls the text from profileName and profileDescription
-});
-profileCloseBtn.addEventListener("click", () => {
-  closePopUp();
 });
 
 profileEditForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  closePopUp();
+  openEditProfileModal();
+  closeModal();
 });
+
 function getCardElement(cardData) {
   const cardElement = template.cloneNode(true);
   const cardImageEl = cardElement.querySelector(".card__image");
